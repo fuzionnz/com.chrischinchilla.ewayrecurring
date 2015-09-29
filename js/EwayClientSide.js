b96184a661862e8432c513dedcb91d6362771d00
Chris Burgess <chris@fuzion.co.nz>
2015-09-29 15:55:04 +1300

    substring() is a method of string objects

--- js/EwayClientSide.js
+++ js/EwayClientSide.js
@@ -3,14 +3,14 @@
  */
 
 function encryptField(field, apiKey) {
-  if (substring(field.val(), 9) == 'eCrypted:') {
+  if (field.val().substring(0, 9) == 'eCrypted:') {
     return;
   }
   field.val(eCrypt.encryptValue(field.val(), apiKey));
 }
 
 //ewayClient(CRM.eway.ewayKey);
-cj('#crm-main-content-wrapper form').submit(function() {
+cj('#crm-main-content-wrapper form').submit(function(ev) {
     encryptField(cj('#credit_card_number'), CRM.eway.ewayKey);
     encryptField(cj('#cvv2'), CRM.eway.ewayKey);
   }
