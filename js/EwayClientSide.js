650c51a1f2ecc8b41c292ed9af1086763636b2ca
eileenmcnaughton <eileen@fuzion.co.nz>
2015-09-30 20:49:14 +0000

    further work on eway client-side-encryption
    This is the code I was still working on locally but had not yet committed, but since there has been a PR done & merged since I started this (that
    covers some of the same group) am committing the WIP

--- js/EwayClientSide.js
+++ js/EwayClientSide.js
@@ -2,15 +2,44 @@
  * Created by eileen on 28/09/2015.
  */
 
+/**
+ * Encrypt confidential data fields before the form is submitted.
+ *
+ * @param field
+ * @param string apiKey
+ */
 function encryptField(field, apiKey) {
-  if (field.val().substring(0, 9) == 'eCrypted:') {
+  var existingValue = field.val();
+  if (isFieldEncrypted(field)) {
     return;
   }
-  field.val(eCrypt.encryptValue(field.val(), apiKey));
+  field.val(eCrypt.encryptValue(existingValue, apiKey));
 }
 
-//ewayClient(CRM.eway.ewayKey);
-cj('#crm-main-content-wrapper form').submit(function(ev) {
+/**
+ * Hide fields that have already been encrypted.
+ */
+function hideEncryptedFields(field) {
+  if (!isFieldEncrypted(field)) {
+    return;
+  }
+  field.hide();
+}
+
+/**
+ * Is the field already encrypted.
+ *
+ * @param field
+ */
+function isFieldEncrypted(field) {
+  var existingValue = field.val();
+  if (existingValue.substr(0, 9) == 'eCrypted:') {
+    return true;
+  }
+  return false;
+}
+
+cj('#crm-main-content-wrapper form').submit(function() {
     encryptField(cj('#credit_card_number'), CRM.eway.ewayKey);
     encryptField(cj('#cvv2'), CRM.eway.ewayKey);
   }
